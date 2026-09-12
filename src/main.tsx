400b1992603799c36d5c36dd36bf9854879cf581
csecouakb <cse.cou.akb@gmail.com>
2026-09-12 11:59:05 +0600

    Wire touch-first timeline enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './editor.css'
+import './timelineEnhancements.css'
+import { installTimelineEnhancements } from './timelineEnhancements'
+
+const cleanupTimelineEnhancements = installTimelineEnhancements()
+window.addEventListener('beforeunload', cleanupTimelineEnhancements, { once: true })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
